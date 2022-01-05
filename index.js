8f472212ca0a22267808ad88203e9199b85c6887
Jane Yushchenko <kelenella@ukr.net>
2022-01-05 18:59:22 +0200

    fixed index

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ const express = require("express");
 const morgan = require("morgan");
 require("dotenv").config();
 const axios = require("axios").default;
+const cors = require("cors");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(morgan("tiny"));
+app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
-// http://api.weatherbit.io/v2.0/current?key=0c3ff6a230224f1b8554fd508218dbf8&lat=46.486970&lon=30.712561
 const thirdPartyBaseUrl = "http://api.weatherbit.io/v2.0/current";
 const thirdPartyApiKey = process.env.WEATHER_API_KEY;
 
-const queryURL = `${thirdPartyBaseUrl}?key=${thirdPartyApiKey}&lat=46.486970&lon=30.712561`;
+const queryURL = `${thirdPartyBaseUrl}?key=${thirdPartyApiKey}&lat=4.486970&lon=-30.712561`;
 app.get("/api/weather", async (req, res) => {
   try {
     const response = await axios.get(queryURL).then((response) => {
       const weatherData = response.data;
-      // const { station } = weatherData;
       console.log(weatherData);
       res.json(weatherData);
     });
